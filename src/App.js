2c23f8a13f17c7a1ad0f20392f05550543032348
Mathieu Delcourt <mathieudelcourt@MacBook-Air-de-Mathieu.local>
2023-07-18 16:35:53 +0200

    pb page about

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React from 'react'
 import { Routes, Route } from "react-router-dom";
 import Home from './pages/Home/Home';
-import Apropos from './pages/Apropos/Apropos';
+import About from './pages/About/about';
 import Error from './pages/Error/Error';
 import Header from "./components/Header/header";
 import Footer from './components/Footer/footer';
+
 
 
 export default function App() {
   return (
     <div className='container'>
       <Header />
-
-    
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/apropos" element={<Apropos />} />
+          <Route path="/About" element={<About />} />
           <Route path="/logement" element={<logement />} />
           <Route path="*" element={<Error />} />
         </Routes>
---
+++ src/components/Cards /cards.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../../components/Cards/cards.scss'
+
+export default function cards() {
+  return (
+    <section className='cards'>
+        <figure className='cards__cont'>
+            <img className='cards__img' src="" alt="" />
+            <figcaption className='cards__title'></figcaption>
+        </figure>
+    </section>
+ 
+  )
+}
