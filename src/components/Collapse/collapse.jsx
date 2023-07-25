702f09aaa7f84f63ab40fdf5417aa0739dcfdedf
Mathieu Delcourt <mathieudelcourt@MacBook-Air-de-Mathieu.local>
2023-07-25 14:40:31 +0200

    collapses page about

--- src/components/Collapse/collapse.jsx
+++ src/components/Collapse/collapse.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import dropUP from '../../assets/dropUP.png'; 
+import dropUP from "../../assets/dropUP.png"
+import'../Collapse/collapse.scss';
 
 export default function Collapse({ title, content }) {
   const [collapse, setCollapse] = useState(true);
@@ -10,8 +11,10 @@ export default function Collapse({ title, content }) {
 
   return (
     <div className='collapse' onClick={toggleCollapse}>
+      <div className='collapse__title__img'>
       <h3>{title}</h3>
-      <img src={dropUP} alt="Drop Up" />
+      <img src={dropUP} alt="Drop Up" className='drop__up' />
+      </div>
       {collapse ? null : <p>{content}</p>}
     </div>
   );
